4c565846e1a864742127528129af7c733315ea9a
Bill Butts <rwbutts@gmail.com>
2025-06-07 21:54:03 -0400

    cleanup

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -2,41 +2,41 @@
 
      <div class='keyboard' >
           <div class='kb-row' >
-               <key char='Q' ref='Q_KEY'/>
-               <key char='W' ref='W_KEY'/>
-               <key char='E' ref='E_KEY'/>
-               <key char='R' ref='R_KEY'/>
-               <key char='T' ref='T_KEY'/>
-               <key char='Y' ref='Y_KEY'/>
-               <key char='U' ref='U_KEY'/>
-               <key char='I' ref='I_KEY'/>
-               <key char='O' ref='O_KEY'/>
-               <key char='P' ref='P_KEY'/>
+               <key char='Q'/>
+               <key char='W'/>
+               <key char='E'/>
+               <key char='R'/>
+               <key char='T'/>
+               <key char='Y'/>
+               <key char='U'/>
+               <key char='I'/>
+               <key char='O'/>
+               <key char='P'/>
           </div>
           <div class='kb-row' >
-               <key char='A' ref='A_KEY'/>
-               <key char='S' ref='S_KEY'/>
-               <key char='D' ref='D_KEY'/>
-               <key char='F' ref='F_KEY'/>
-               <key char='G' ref='G_KEY'/>
-               <key char='H' ref='H_KEY'/>
-               <key char='J' ref='J_KEY'/>
-               <key char='K' ref='K_KEY'/>
-               <key char='L' ref='L_KEY'/>
+               <key char='A'/>
+               <key char='S'/>
+               <key char='D'/>
+               <key char='F'/>
+               <key char='G'/>
+               <key char='H'/>
+               <key char='J'/>
+               <key char='K'/>
+               <key char='L'/>
           </div>
           <div class='kb-row' >
-               <key char='DELETE' ref='DELETE_KEY' :controlKey="true" label='DELETE'/>
-               <key char='Z' ref='Z_KEY'/>
-               <key char='X' ref='X_KEY'/>
-               <key char='C' ref='C_KEY'/>
-               <key char='V' ref='V_KEY' />
-               <key char='B' ref='B_KEY'/>
-               <key char='N' ref='N_KEY'/>
-               <key char='M' ref='M_KEY'/>
-               <key char='ENTER' ref='ENTER_KEY'   :controlKey="true" label='ENTER'/>
+               <key char='DELETE' :controlKey="true" label='DELETE'/>
+               <key char='Z'/>
+               <key char='X'/>
+               <key char='C'/>
+               <key char='V' />
+               <key char='B'/>
+               <key char='N'/>
+               <key char='M'/>
+               <key char='ENTER'  :controlKey="true" label='ENTER'/>
          </div>
          <div class='kb-row' >
-               <key  ref='RESET_KEY' char='RESET'  :controlKey="true" label='PLAY AGAIN' @click="broadcastReload();" />
+               <key  char='RESET'  :controlKey="true" label='PLAY AGAIN' @click="broadcastReload();" />
          </div>
      </div>
      
@@ -48,7 +48,6 @@
 import Vue  from 'vue'
 import Key from './Key.vue'
 import EventBus from '../EventBus'
-import { MatchCodes, IClassifiedKeyRefs, KeyCodes,  } from '@/types';
 
 export default Vue.extend({
      name: 'keyboard',
@@ -58,54 +57,12 @@ export default Vue.extend({
           return {
           };
      },
-
      components: {
           Key
-     },
-     computed: {
-            allKeyRefs(): IClassifiedKeyRefs {
-                let result: IClassifiedKeyRefs = { all:[] , alpha:[], nonalpha:[]};
-                Object.keys(this.$refs).filter( k => k.endsWith('_KEY')).forEach ( k=>{
-                    let keyObj = this.$refs[k];
-                    if(!keyObj) {
-                        console.error(`got undefined $ref[${k}]`);
-                    } else {
-                        result.all.push(k);
-                        if ((keyObj as InstanceType<typeof Key>).$el?.hasAttribute('control_key')) {
-                            result.nonalpha.push(k);
-                        } else {
-                            result.alpha.push(k);
-                        }
-                    }
-                });
-                console.log(result);
-                return result;
-            },
      },
      methods: {
         broadcastReload(){
             EventBus.emitTriggerWordLoadEvent();
-        },
-        setKeyColor( key:string|KeyCodes, color : MatchCodes) {
-            let keyList: string[];
-            switch(true) {
-                case key===KeyCodes.ALL:
-                    keyList = this.allKeyRefs.all;
-                    break;
-                case key===KeyCodes.ALPHA:
-                    keyList = this.allKeyRefs.alpha;
-                    break;
-                case key===KeyCodes.NONALPHA:
-                    keyList = this.allKeyRefs.nonalpha;
-                    break;
-                case ['DELETE', 'ENTER', 'RESET'].includes(key) || (key>='A' && key<='Z' && key.length==1):
-                    keyList = [key];
-                    break;
-                default:
-                    keyList = [];
-                    console.error(`keyboard::setKeyColor{} unknown class key ${key}`);
-            }
-            keyList.forEach( (k) => {(this.$refs[k] as InstanceType<typeof Key>)?.setKeyColor(color);} );
         },
      },
 });
